bdb42395ed3dbca6116dd994dd34d32549f76d27
UncleZheng1 <33140954+UncleZheng1@users.noreply.github.com>
2018-01-11 18:11:11 +0800

    房间号输入、扫一扫

--- pages/actions/actions.js
+++ pages/actions/actions.js
@@ -1,15 +1,45 @@
 const util = require('../../utils/util.js')
-
+var QR = require("../../utils/qrcode.js");
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    qrcPhld:"1",
+
   },
+  size: {
+    w: 0,
+    h: 0
+  },
+  canvasId:"qrcCanvas",
   onLoad: function () {
     // this.setData({
     //   logs: (wx.getStorageSync('logs') || []).map(log => {
     //     return util.formatTime(new Date(log))
     //   })
     // })
-  }
+    this.size = this.setCanvasSize();//动态设置画布大小  
+    this.createQrCode(this.data.qrcPhld, this.canvasId, this.size.w, this.size.h);
+  },
+  createQrCode: function (str, canvasId, cavW, cavH) {
+    //调用插件中的draw方法，绘制二维码图片  
+    QR.api.draw(str, canvasId, cavW, cavH);
+
+  },
+  //适配不同屏幕大小的canvas  
+  setCanvasSize: function () {
+    var size = {};
+    try {
+      var res = wx.getSystemInfoSync();
+      var scale = 750 / 300;//不同屏幕下canvas的适配比例；设计稿是750宽  
+      var width = res.windowWidth / scale;
+      var height = width;//canvas画布为正方形  
+      size.w = width;
+      size.h = height;
+    } catch (e) {
+      // Do something when catch error  
+      console.log("获取设备信息失败" + e);
+    }
+    return size;
+  }, 
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,7 +3,10 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    inputString:"",
+    bindNumberArray:[],
+    isFocus:false,
   },
   onLoad: function () {
     // this.setData({
@@ -11,5 +14,34 @@ Page({
     //     return util.formatTime(new Date(log))
     //   })
     // })
+  },
+  roomCodeInputListener:function(e){
+    console.log(e);
+    if (e.detail.cursor <= 4 && e.detail.cursor >= 0){
+      if (e.detail.value.length === 0){
+        this.data.bindNumberArray = [];
+      }else{
+        this.data.bindNumberArray = e.detail.value.split("");
+      }
+      
+      console.log("this.data.bindNumberArray=" + this.data.bindNumberArray);
+      this.setData({
+        bindNumberArray: this.data.bindNumberArray
+      })
+    }
+  },
+  set_Focus:function(){
+    this.setData({
+      isFocus: true,
+    })
+  },
+  saomiao:function(){
+    // 只允许从相机扫码
+    wx.scanCode({
+      onlyFromCamera: true,
+      success: (res) => {
+        console.log(res)
+      }
+    })
   }
 })
