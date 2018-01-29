b546bc9b78348123310af9cd6ccd34af72c09cd4
胡义晋 <jingdiqkl123@qq.com>
2018-01-29 16:10:03 +0800

    二维码

--- pages/BillNew/BillNew.js
+++ pages/BillNew/BillNew.js
@@ -1,12 +1,13 @@
 const util = require('../../common/util.js')
 const url = require('../../common/constant_url.js')
-const app = getApp()
+const QR = require('../../common/qrcode.js')
 
 Page({
   data: {
-  
+    
   },
   onLoad: function (options) {
+    
     util.http_post(url.CreateAction, {}, res => {
       if(res.success){
         this.setData({
@@ -14,7 +15,30 @@ Page({
           id: res.data.id,
           title: res.data.title
         })
+        this.drawQr()
       }
     })
+  },
+  drawQr: function(){
+    var size = this.setCanvasSize()
+    const qrdata = this.data.id
+    this.createQrCode(qrdata, "mycanvas", size.w, size.h)
+  },
+  setCanvasSize: function () {
+    var size = {};
+    try {
+      var res = wx.getSystemInfoSync();
+      var scale = 750 / 200;
+      var width = res.windowWidth / scale;
+      var height = width;
+      size.w = width;
+      size.h = height;
+    } catch (e) {
+      console.log("获取设备信息失败" + e);
+    }
+    return size;
+  },
+  createQrCode: function (url, canvasId, cavW, cavH) {
+    QR.api.draw(url, canvasId, cavW, cavH);
   },
 })
