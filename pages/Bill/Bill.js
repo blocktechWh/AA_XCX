a31d64d475c0a154ddd4f270cd8d51838764df9c
胡义晋 <jingdiqkl123@qq.com>
2018-01-30 20:34:00 +0800

    卡片滑动

--- pages/Bill/Bill.js
+++ pages/Bill/Bill.js
@@ -3,9 +3,9 @@ const url = require('../../common/constant_url.js')
 
 Page({
   data: {
-    loaded:false,
+    loaded: false,
     items: [],
-    currentIndex:1,
+    currentIndex: 0,
   },
   onLoad: function (options) {
     this.setData({ actionId: options.id })
@@ -23,5 +23,32 @@ Page({
   },
   toDetailNew: function(){
     wx.navigateTo({ url: '/pages/DetailNew/DetailNew?id=' + this.data.actionId })
+  },
+  touchstartX: 0,
+  touchstart: function (e) {
+    this.touchstartX = e.touches[0].clientX
+  },
+  touchend: function (e) {
+    const offsetX = e.changedTouches[0].clientX - this.touchstartX
+    if (offsetX > 50){
+      this.prev()
+    } else if (offsetX < -50){
+      this.next()
+    }
+    this.touchstartX = 0;
+  },
+  prev: function () {
+    if (this.data.currentIndex == 0) {
+      return
+    } else {
+      this.setData({ currentIndex: this.data.currentIndex - 1 })
+    }
+  },
+  next: function () {
+    if (this.data.currentIndex >= this.data.items.length-1) {
+      return
+    } else {
+      this.setData({ currentIndex: this.data.currentIndex + 1 })
+    }
   }
 })
